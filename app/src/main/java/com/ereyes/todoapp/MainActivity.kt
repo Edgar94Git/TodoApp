30b2f3b61ef595827a764fe4479fa049d61156f1
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-03-25 11:44:37 -0600

    uploaded adapter

--- app/src/main/java/com/ereyes/todoapp/MainActivity.kt
+++ app/src/main/java/com/ereyes/todoapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.ereyes.todoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.ereyes.todoapp.databinding.ActivityMainBinding
+import com.ereyes.todoapp.Category.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val categories = listOf(
+        Business,
+        Personal,
+        Other
+    )
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var categoryAdapter: CategoryAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setUpAdapter()
+    }
+
+    private fun setUpAdapter(){
+        categoryAdapter = CategoryAdapter(categories)
+        binding.rvCategories.adapter = categoryAdapter
+        binding.rvCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
     }
 }
