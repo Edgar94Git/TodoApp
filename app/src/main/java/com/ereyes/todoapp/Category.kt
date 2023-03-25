b54be24ac195a7f71d5e4f6f7235d124870aab52
Edgar Reyes Gonzalez <gzzenit@gmail.com>
2023-03-25 11:43:38 -0600

    sealed class Category

---
+++ app/src/main/java/com/ereyes/todoapp/Category.kt
@@ -0,0 +1,13 @@
+package com.ereyes.todoapp
+
+/****
+ * Project: TodoApp
+ * From: com.ereyes.todoapp
+ * Created by Edgar Reyes Gonzalez on 3/25/2023 at 9:20 AM
+ * All rights reserved 2023.
+ ****/
+sealed class Category{
+    object Personal : Category()
+    object Business : Category()
+    object Other : Category()
+}
